4edcc3901e6a58486f5b3b62759b2f669c13136a
aemonge <andres.monge@gmail.com>
2017-02-16 13:43:43 -0600

    feat(all): Included a basic test

---
+++ test/prettyjson_spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('should');
+var pathify = require('../pathify');
+
+/* global describe it */
+
+describe('pathify general tests', function() {
+  it('Example it', function() {
+    (true).should.equal(true);
+  });
+});
